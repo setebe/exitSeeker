988000a324abd6464b57bab0baab8a8b16200e55
Ilhomjon <ilhomparisi@yandex.com>
2020-11-29 04:52:12 +0500

    user agent manipulation

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const agent = new https.Agent({
     rejectUnauthorized: false,
     protocol: 'http',
     host: '127.0.0.1',
-    port: '24000'
+    port: '9090'
   });
-  axios.get('amazon.com/dp/B00X17TCMG', { httpsAgent: agent }).then(res=>{
+  axios.get('https://wtfismyip.com/json', { headers: { 'User-Agent': 'Console app' }, httpsAgent: agent }).then(res=>{
       console.log(res.data)
   }).catch(e=>{
       console.error(e)
