56a6fb4911f3430c8601d521bff4f21934751d2a
Ilhomjon <ilhomparisi@yandex.com>
2020-11-29 04:25:54 +0500

    ip test

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const axios = require('axios');
+const axios = require('axios')
+const SocksAgent = require('axios-socks5-agent')
+ 
+const { httpAgent, httpsAgent } = new SocksAgent({ host: '127.0.0.1', port: 24000, agentOptions: { keepAlive: true } }) 
+axios
+  .get('http://wtfismyip.com/json', { httpAgent, httpsAgent })
+  .then(res => console.log(res.data))
+  .catch(e => console.error(e))
