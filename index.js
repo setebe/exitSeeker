0f05c1bdf6c3c9a344bc18bd391c4da1efee81de
Ilhomjon <ilhomparisi@yandex.com>
2020-11-29 04:41:58 +0500

    target address to amazon

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 const axios = require('axios')
 const SocksAgent = require('axios-socks5-agent')
 const https = require('https');
- 
-// const { httpAgent, httpsAgent } = new SocksAgent({ rejectUnauthorized:false, host: '127.0.0.1', port: 24000, agentOptions: { keepAlive: true } }) 
-// axios
-//   .get('https://wtfismyip.com/json', { httpAgent, httpsAgent })
-//   .then(res => console.log(res.data))
-//   .catch(e => console.error(e))
-
 
 const agent = new https.Agent({  
     rejectUnauthorized: false,
@@ -15,7 +8,7 @@ const agent = new https.Agent({
     host: '127.0.0.1',
     port: '24000'
   });
-  axios.get('https://wtfismyip.com/json', { httpsAgent: agent }).then(res=>{
+  axios.get('https://amazon.com', { httpsAgent: agent }).then(res=>{
       console.log(res.data)
   }).catch(e=>{
       console.error(e)
