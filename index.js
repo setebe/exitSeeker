bad951670b348ba59b17ef477128b2c58fabb3d5
Ilhomjon <ilhomparisi@yandex.com>
2020-11-29 04:58:57 +0500

    to https

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const https = require('https');
 
 const agent = new https.Agent({  
     rejectUnauthorized: false,
-    protocol: 'http',
+    protocol: 'https',
     host: '127.0.0.1',
-    port: '24000'
+    port: '9090'
   });
-  axios.get('https://wtfismyip.com/json', { headers: { 'User-Agent': 'Console app' }, httpsAgent: agent }).then(res=>{
+  axios.get('https://amazon.com/dp/B00X17TCMG', { headers: { 'User-Agent': 'Console app' }, httpsAgent: agent }).then(res=>{
       console.log(res.data)
   }).catch(e=>{
       console.error(e)
