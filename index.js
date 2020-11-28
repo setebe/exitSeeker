e7dbaf65e321b15758894b0e9fca1f5a1c1c4862
Ilhomjon <ilhomparisi@yandex.com>
2020-11-29 04:32:13 +0500

    reject added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 const SocksAgent = require('axios-socks5-agent')
  
-const { httpAgent, httpsAgent } = new SocksAgent({ host: '127.0.0.1', port: 24000, agentOptions: { keepAlive: true } }) 
+const { httpAgent, httpsAgent } = new SocksAgent({ rejectUnauthorized:false, host: '127.0.0.1', port: 24000, agentOptions: { keepAlive: true } }) 
 axios
   .get('https://wtfismyip.com/json', { httpAgent, httpsAgent })
   .then(res => console.log(res.data))
