11b816d537f3fac9e02d01bf77916f3814b478df
Ilhomjon <ilhomparisi@yandex.com>
2020-11-29 06:48:04 +0500

    test cookie

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const agent = new https.Agent({
     host: '127.0.0.1',
     port: '24000'
   });
-  axios.get('https://www.amazon.com/dp/B00X17TCMG',  {httpsAgent: agent, maxRedirects:5, withCredentials: true }).then(res=>{
+  axios.get('https://www.amazon.com/dp/B00X17TCMG',   { Headers:{Cookie:"skin=noskin; path=/; domain=.amazon.com"} ,httpsAgent: agent, maxRedirects:5, withCredentials: true }).then(res=>{
       console.log(res.data)
   }).catch(e=>{
       console.error(e)
